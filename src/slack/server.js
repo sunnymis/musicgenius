0c707e0f1d360edcc37c46fe8bdc7855c0314a9d
Sunny Mistry <sunny.mistry@policygenius.com>
2020-06-17 23:14:15 -0400

    Cleanup Slack Server Code

--- src/slack/server.js
+++ src/slack/server.js
@@ -2,23 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const { createEventAdapter } = require('@slack/events-api');
-const app = express();
-const port = 4000;
-const port2 = 5000;
 const constants = require('../constants');
+const app = express();
+const portEvents = 4000;
+const portCommands = 5000;
 
 const signingSecret = process.env.SLACK_SIGNING_SECRET;
 const slackEvents = createEventAdapter(signingSecret);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 (async () => {
-  // Start the built-in server
-  const server = await slackEvents.start(port);
-  // Log a message when the server is ready
-  console.log(`Listening for events on ${server.address().port}`);
+  const server = await slackEvents.start(portEvents);
+  console.log(`Listening for events on ${server.address().portEvents}`);
 })();
 
+app.listen(portCommands, () => {
+  console.log(`Slack Server started. Listening on http://localhost:${portCommands}`)
+});
+
+
 slackEvents.on('message', (event) => {
-  console.log('Event', event);
   if (event.type === 'message') {
     if (event.text && event.text.includes("https://open.spotify.com/track")) {
       axios({
@@ -28,28 +33,17 @@ slackEvents.on('message', (event) => {
           song: event.text
         }
       })
-        .then(resp => console.log('succ resp', resp))
+        .then(resp => console.log('success resp', resp))
         .catch(err => console.log('err', err));
     }
   }
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 });
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-app.listen(port2, () => {
-  console.log(`Slack Server started. Listening on http://localhost:${port2}`)
-});
 
 app.post('/command', (req, res) => {
-  // todo make checks for team domain and channel name  in req.body
   const { text } = req.body;
-
   const [name, description] = text.split(',');
 
-  console.log('name description', name, description);
-  console.log('url', constants.CREATE_PLAYLIST_URL);
   axios({
     url: `${constants.CREATE_PLAYLIST_URL}`,
     method: 'post',
